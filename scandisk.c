e70216370fc58a2e85305061ee82ae978556db6d
Jiayang (JY) Li <jli1@colgate.edu>
2014-12-11 03:45:53 -0500

    able to print incompatible size issues

--- scandisk.c
+++ scandisk.c
@@ -8,6 +8,8 @@
 #include <sys/stat.h>
 #include <string.h>
 
+#include <assert.h>
+
 #include "bootsect.h"
 #include "bpb.h"
 #include "direntry.h"
@@ -15,9 +17,41 @@
 #include "dos.h"
 
 #define TOTAL_CLUSTERS(bpb) (bpb->bpbSectors / bpb->bpbSecPerClust)
+#define CLUSTER_SIZE(bpb) (bpb->bpbSecPerClust * bpb->bpbBytesPerSec)
 
-int follow_file(uint16_t cluster, uint8_t *img_buf, struct bpb33 *bpb){
+void follow_file(uint16_t cluster, uint32_t size, uint8_t *img_buf, struct bpb33 *bpb, char *ref_clusters){
+    uint32_t size_from_dirent = size;
+    uint16_t last_fat_entry = 0;
+    uint32_t chain_size = 0;
+    //printf("before size: %d\n", size);
+    
+    //assert(cluster != 0);
+    while (is_valid_cluster(cluster, bpb)){
+        /* !!! mark this cluster referenced here !!! */
 
+        chain_size += CLUSTER_SIZE(bpb);
+
+        if (size < CLUSTER_SIZE(bpb)){ //should be the last cluster according to dirent size
+            last_fat_entry = cluster;
+            cluster = get_fat_entry(cluster, img_buf, bpb);
+            break;
+        }
+        size -= CLUSTER_SIZE(bpb);
+
+        cluster = get_fat_entry(cluster, img_buf, bpb);
+    }
+
+    // printf("cluster number: %d\n", cluster);
+    //printf("chain size: %d\n", chain_size);
+    if (is_valid_cluster(cluster, bpb)){    //still in the middle of a chain, free following clusters
+        /* !!! free clusters beginning from cluster !!! */
+        printf("chain size (%d) greater than dirent size (%d)\n", chain_size, size_from_dirent);
+    } else if (size_from_dirent > chain_size){  //reached the end of chain, but dirent size is still too big
+        /* !!! adjust dirent size !!! */
+        printf("chain size (%d) less than dirent size (%d)\n", chain_size, size_from_dirent);
+    } else {
+        printf("normal file!\n");
+    }
 }
 
 /* parse a given dirent, returns the starting cluster if the given
@@ -60,27 +94,52 @@ uint16_t parse_dirent(struct direntry *dirent, uint8_t *img_buf, struct bpb33 *b
         break;
     }
 
+    // if ((dirent->deAttributes & ATTR_NORMAL) == ATTR_NORMAL){   //"regular" file
+    //     uint32_t size_from_dirent = getulong(dirent->deFileSize);
+    //     printf("starting cluster %d\n", getushort(dirent->deStartCluster));
+    //     follow_file(getushort(dirent->deStartCluster), size_from_dirent, img_buf, bpb, ref_clusters);
+    // } else if ((dirent->deAttributes & ATTR_DIRECTORY) != 0){    
+    //     if ((dirent->deAttributes & ATTR_HIDDEN) != ATTR_HIDDEN){
+    //         //a (non-hidden) dir entry
+    //         subdir_cluster = getushort(dirent->deStartCluster);
+    //     }
+    // } 
+
     if ((dirent->deAttributes & ATTR_WIN95LFN) == ATTR_WIN95LFN){
-    // ignore any long file name extension entries
-    //
-    // printf("Win95 long-filename entry seq 0x%0x\n", dirent->deName[0]);
-    } else if ((dirent->deAttributes & ATTR_DIRECTORY) != 0){    
+        /* skip long file name */
+    } else if ((dirent->deAttributes & ATTR_VOLUME) != 0){
+        /* skip volume */
+    } else if ((dirent->deAttributes & ATTR_DIRECTORY) != 0){
+        /* skip hidden dir */
         if ((dirent->deAttributes & ATTR_HIDDEN) != ATTR_HIDDEN){
-            //a (non-hidden) dir entry
+            // a normal dir
             subdir_cluster = getushort(dirent->deStartCluster);
         }
     } else {
-        //a "regular" file entry
+        // a normal file
         uint32_t size_from_dirent = getulong(dirent->deFileSize);
-        int size_from_chain = follow_file(dirent->deStartCluster, img_buf, bpb);
-        /* !!! do size fix here !!! */
+        uint16_t starting_cluster = getushort(dirent->deStartCluster);
+        follow_file(starting_cluster, size_from_dirent, img_buf, bpb, ref_clusters);
     }
 
     return subdir_cluster;
 }
 
-void follow_dir(uint16_t cluster_num, uint8_t *img_buf, struct bpb33 *bpb, char *ref_clusters){
+void follow_dir(uint16_t cluster, uint8_t *img_buf, struct bpb33 *bpb, char *ref_clusters){
+    while (is_valid_cluster(cluster, bpb)){
+        /* !!! mark this cluster referenced here !!! */
+        struct direntry *dirent = (struct direntry *) cluster_to_addr(cluster, img_buf, bpb);
 
+        int numDirEntries = (bpb->bpbBytesPerSec * bpb->bpbSecPerClust) / sizeof(struct direntry);
+        for (int i = 0; i < numDirEntries; i++){    //parse every direntry and follow subdir if any
+            uint16_t subdir_cluster = parse_dirent(dirent, img_buf, bpb, ref_clusters);
+            if (subdir_cluster){
+                follow_dir(subdir_cluster, img_buf, bpb, ref_clusters);
+            }
+            dirent++;
+        }
+        cluster = get_fat_entry(cluster, img_buf, bpb);
+    }
 }
 
 char *traverse_root(uint8_t *img_buf, struct bpb33 *bpb){
@@ -92,9 +151,8 @@ char *traverse_root(uint8_t *img_buf, struct bpb33 *bpb){
     char *ref_clusters = (char *) malloc(sizeof(char) * TOTAL_CLUSTERS(bpb));
     memset(ref_clusters, 0, sizeof(char) * TOTAL_CLUSTERS(bpb));
 
-    int i = 0;
-    for (; i < bpb->bpbRootDirEnts ;i++){   //go to every entry in root dir
-        uint16_t subdir_cluster = parse_dirent(dirent, ref_clusters);
+    for (int i = 0; i < bpb->bpbRootDirEnts ;i++){   //go through every entry in root dir
+        uint16_t subdir_cluster = parse_dirent(dirent, img_buf, bpb, ref_clusters);
         if (is_valid_cluster(subdir_cluster, bpb)){
             follow_dir(subdir_cluster, img_buf, bpb, ref_clusters);
         }
@@ -102,6 +160,7 @@ char *traverse_root(uint8_t *img_buf, struct bpb33 *bpb){
     }
     return ref_clusters;
 }
+
 
 void usage(char *progname) {
     fprintf(stderr, "usage: %s <imagename>\n", progname);
@@ -117,7 +176,7 @@ int main(int argc, char** argv) {
     char *ref_clusters;
 
     if (argc < 2) {
-	   usage(argv[0]);
+       usage(argv[0]);
     }
 
     img_buf = mmap_file(argv[1], &fd);
@@ -125,6 +184,8 @@ int main(int argc, char** argv) {
     
     // your code should start here...
     ref_clusters = traverse_root(img_buf, bpb);
+    
+    
 
     unmmap_file(img_buf, &fd);
     free(ref_clusters);
